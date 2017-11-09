9711779902c16b99e410b9c95a8459d3493edc24
pimgeek <wudi.biz@gmail.com>
2017-11-09 13:40:45 +0800

    修改 bookmarklet 源码与目标码，同时适配 Workflowy 和 Dynalist

--- src/clip-as-opml-tmpl.js
+++ src/clip-as-opml-tmpl.js
@@ -1,51 +1,43 @@
 javascript: (function() {
-  var inbox = 'https://workflowy.com/#';
+  var inbox = '#';
 
-  function eopml(i) {
+  function escape_chars(i) {
     var o = i.replace(/&/g, '&amp;amp;').replace(/</g, '&amp;lt;').replace(/>/g, '&amp;gt;').replace(/"/g, '&quot;').replace(/(\n)/g, '&#10;');
     return (o);
   }
 
-  var docTitle = eopml(document.title);
-  var docUrl = eopml(location.href) + ' ';
-  var docSelection = eopml(window.getSelection().toString());
-  console.log(docSelection);
-  var docSelectionSplitIdx = docSelection.search(/([?!。？！]|&#10;)/);
+  var docTitle = escape_chars(document.title);
+  var docUrl = escape_chars(location.href) + ' ';
+  var docSelection = escape_chars(window.getSelection().toString());
+  var docSelectionSplitIdx = docSelection.search(/([?!。？！…]|&#10;)/);
   var docSelectionHeader = docSelection.substring(0, docSelectionSplitIdx);
-  var isWorkFlowy = location.href.indexOf('workflowy.com/#');
+  var isWorkFlowy = location.href.indexOf('workflowy.com/');
+  var isDynalist = location.href.indexOf('dynalist.io/');
   var isChrome = !!window.chrome;
 
   if (isWorkFlowy !== -1) {
-    var docTitle = '&lt;i&gt;参见&quot;' +
+    var docTitle = '参见另一条目 &quot;' +
       docTitle.replace(/ - WorkFlowy$/, '') +
-      '&quot;&lt;/i&gt;';
+      '&quot;';
   }
 
-  var clip = '<?xml version="1.0"?>' +
+  if (isDynalist !== -1) {
+    var docTitle = '参见另一条目 &quot;' +
+      docTitle.replace(/ - Dynalist$/, '') +
+      '&quot;';
+  }
+
+  var clip =
+    '<?xml version="1.0"?>' +
     '<opml version="2.0">' +
-    '  <head>' +
-    '    <ownerEmail>anonymous@hintsnet.com</ownerEmail>' +
-    '  </head>' +
     '  <body>' + 
-    '    <outline text="|->| #hint-pipe #to-refine " >' +
-    '      <outline text="' + docSelectionHeader + '" _note="' + docSelection + '" />' +
-    '      <outline text="' + docTitle + '" _note="' + docUrl +'" /></outline>' +
-    '    </outline>' +
+    '    <outline text="' + docTitle + '" _note="' + docSelection + '&#10;[参考链接](' + docUrl + ' )" />' +
     '  </body>' +
     '</opml>';
 
   if (isChrome && clip.length > 2000) {
-    window.open('' +
-      inbox +
-      '/?q=' +
-      encodeURIComponent(clip) +
-      '');
+    window.open('' + inbox + '/?q=' + encodeURIComponent(clip) + '');
   } else {
-    var userInput = prompt('  请复制下列文字 \(Ctrl + C\) 或 \(Cmd + C\) \n\n  并打开 Workflowy 页签粘贴为新的笔记条目\n', clip);
-    if (userInput !== null) {
-      if (isWorkFlowy !== -1) { location.href = inbox; } else { ; }
-    } else {
-      return;
-    }
+    prompt('  请复制下列文字 \(Ctrl-C 或 Cmd-C\) \n\n  并打开 Workflowy / Dynalist 页签粘贴为新的笔记条目\n', clip);
   }
 })()
